b8f6bc9a4a2f8510554651e1406d28d02238b422
dhanushthi64 <dhanushthillai64@gmail.com>
2024-03-23 12:30:57 +0530

    First Commit

---
+++ DessertClicker/app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.model
+
+import androidx.annotation.DrawableRes
+
+data class Dessert(
+    @DrawableRes val imageId: Int,
+    val price: Int,
+    val startProductionAmount: Int
+)
